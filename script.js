eca2619f6dd77463c3cce7127d8c3e3d5e1c20a8
SRIYA CHAKINALA <142564481+Sriya77@users.noreply.github.com>
2025-05-02 15:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let fn = document.getElementById("fn");
+let ln = document.getElementById("ln");
+let pn = document.getElementById("pn");
+let email = document.getElementById("email");
+
+function alertt() {
+	alert(`First Name: ${fn.value}, Last Name: ${ln.value}, Number: ${pn.value}, Email ID: ${email.value}`);
+}
+
+document.getElementById("submit").addEventListener("click", alertt)
+
